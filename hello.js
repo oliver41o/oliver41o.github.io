bf48c3430f7affadb561d318cb37bd6c18cf911d
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-04 07:12:44 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
 function load(e) {
-    if (!(e.value === "peter is stinky")) {
+    if (!(e. === "peter is stinky")) {
         console.log(window.location.href);
         console.log(e.value);
-        window.location.href = `https://www.google.com/search?q=${e.value}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
-    } else if (e.value === "secret") {
+        window.location.href = `https://www.google.com/search?q=${e}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
+    } else if (e === "secret") {
         window.location.href = "secret";
     } else {
         console.log("peter is stinky");
