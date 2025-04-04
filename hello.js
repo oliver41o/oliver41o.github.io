aaaa8ccf7b9608e7f71eec858b4e43c839c69b8d
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-03 18:18:48 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,19 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let e = document.createElement("button");
-    let e2 = document.createElement("input");
-
-    e.textContent = "search";
-
-    e.addEventListener("click", () => {
-        if (!(e2.value === "peter is stinky")) {
-            console.log(window.location.href);
-            console.log(e2.value);
-            window.location.href = `https://www.google.com/search?q=${e2.value}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
-        } else {
-            console.log("peter is stinky");
-        };
-    });
-
-    document.body.appendChild(e);
-    document.body.appendChild(e2);
-});
+function e() {
+    if (!(e2.value === "peter is stinky")) {
+        console.log(window.location.href);
+        console.log(e2.value);
+        window.location.href = `https://www.google.com/search?q=${e2.value}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
+    } else {
+        console.log("peter is stinky");
+    };
+};
