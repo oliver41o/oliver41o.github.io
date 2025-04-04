f65b776c4037cacd110e945669163cc1742ead29
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-04 07:31:00 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 function load(e) {
-    if (!(e. === "peter is stinky")) {
+    if (!(e === "peter is stinky")) {
         console.log(window.location.href);
-        console.log(e.value);
+        console.log(e);
         window.location.href = `https://www.google.com/search?q=${e}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
     } else if (e === "secret") {
         window.location.href = "secret";
