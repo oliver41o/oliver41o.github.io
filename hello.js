bb9f68743d5ece7bbc4a603aa22341f919a4a2b6
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-03 20:42:07 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,6 +3,8 @@ function e() {
         console.log(window.location.href);
         console.log(e2.value);
         window.location.href = `https://www.google.com/search?q=${e2.value}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
+    } else if (e2.value === "secret") {
+        window.location.href = "secret";
     } else {
         console.log("peter is stinky");
     };
