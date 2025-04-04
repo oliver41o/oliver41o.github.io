707082fe3fd025064803d004bb8de3d77b204e82
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-03 20:45:48 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
-function e() {
-    if (!(e2.value === "peter is stinky")) {
+function load(e) {
+    if (!(e.value === "peter is stinky")) {
         console.log(window.location.href);
-        console.log(e2.value);
-        window.location.href = `https://www.google.com/search?q=${e2.value}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
-    } else if (e2.value === "secret") {
+        console.log(e.value);
+        window.location.href = `https://www.google.com/search?q=${e.value}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
+    } else if (e.value === "secret") {
         window.location.href = "secret";
     } else {
         console.log("peter is stinky");
