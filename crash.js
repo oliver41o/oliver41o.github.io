7d6a39d3d5d199abed00242e246392ec188f22c4
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-04 14:33:50 -0600

    Update crash.js

--- crash.js
+++ crash.js
@@ -1,3 +1,10 @@
 function crashpc() {
- for (var i = 5; i > 3; i = i + 1){ console.log(i); }
+ for (var i = 5; i > 3; i = i + 1) {
+    var x = document.createElement("div");
+    x.innerText = i;
+  
+    document.body.appendChild(x)
+    console.log(txt);
+    console.log(i); 
+ };
 };
