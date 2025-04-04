775eb4ca85410b6ab941ae5adb38dbe082cd5528
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-04 13:39:50 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,6 +3,10 @@ function load(e) {
         window.location.href = "secret";
     } else if (e === "peter is stinky") {
         console.log("peter is stinky");
+    } else if (e.includes("https://") {
+        window.location.href = e
+    } else if (e === "felix" && document.getElementById("clickerbox").value === 69) {
+        document.getElementById("secretbox").style.visibility = true;
     } else {
         window.location.href = `https://www.google.com/search?q=${encodeURIComponent(e)}&safe=active`;
     }
