229e648fa15dc130c8eae64e5635cc2483669787
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-04 07:34:33 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,11 +1,9 @@
 function load(e) {
-    if (!(e === "peter is stinky")) {
-        console.log(window.location.href);
-        console.log(e);
-        window.location.href = `https://www.google.com/search?q=${e}&rlz=1CAQMZZ_enCA1152&oq=e&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg7MhAIAxAuGMcBGLEDGNEDGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQcxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on`;
-    } else if (e === "secret") {
+    if (e === "secret") {
         window.location.href = "secret";
-    } else {
+    } else if (e === "peter is stinky") {
         console.log("peter is stinky");
-    };
-};
+    } else {
+        window.location.href = `https://www.google.com/search?q=${encodeURIComponent(e)}&safe=active`;
+    }
+}
