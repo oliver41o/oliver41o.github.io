4375849c026640e246fae258cea0aad76caa5b31
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-05 19:28:45 -0600

    Update iframe-search.js

--- iframe-search.js
+++ iframe-search.js
@@ -8,4 +8,18 @@ function iframe_search(e) {
     document.body.appendChild(br);
     document.body.appendChild(br);
     document.body.appendChild(iframe);
+
+    if (e === "secret") {
+        iframe.src = "https://olivery4.github.io/secret";
+    } else if (e === "peter is stinky") {
+        console.log("peter is stinky");
+    } else if (e.includes("https://")) {
+        iframe.src = e;
+    } else if (e === "felix" && document.getElementById("clickerbox").value === "69") {
+        document.getElementById("secretbox").style.display = "inline-block";
+    } else if (e === "music") {
+        iframe.src = "https://olivery4.github.io/music";
+    } else {
+        iframe.src = `https://www.google.com/search?q=${encodeURIComponent(e)}&safe=active`;
+    };
 };
