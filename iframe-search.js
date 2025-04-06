7f573d6c45e413072c6b16f2f86c6866406425dd
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-05 19:24:41 -0600

    Update iframe-search.js

--- iframe-search.js
+++ iframe-search.js
@@ -1,5 +1,8 @@
 function iframe_search(e) {
     const iframe = document.createElement("iframe");
+    const br = document.createElement("br");
 
+    document.body.appendChild(br);
+    document.body.appendChild(br);
     document.body.appendChild(iframe);
 };
